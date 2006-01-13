7ff4a2937ae2d7220cb2683b65542f148006b7e9
David Iberri <diberri@cpan.org>
2006-01-13 21:59:02 +0000

    Import of DIBERRI/Bundle-WikiConverter-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-WikiConverter
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DIBERRI/Bundle-WikiConverter-0.01.tar.gz
    gitpan-cpan-author:       DIBERRI
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/WikiConverter.pm
@@ -0,0 +1,63 @@
+package Bundle::WikiConverter;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::WikiConverter - A bundle to install HTML::WikiConverter and all its dialects
+
+=head1 SYNOPSIS
+
+ C<perl -MCPAN -e 'install Bundle::WikiConverter'>
+
+=head1 CONTENTS
+
+HTML::WikiConverter 0.50
+
+HTML::WikiConverter::DokuWiki
+
+HTML::WikiConverter::Kwiki
+
+HTML::WikiConverter::MediaWiki
+
+HTML::WikiConverter::MoinMoin
+
+HTML::WikiConverter::Oddmuse
+
+HTML::WikiConverter::PhpWiki
+
+HTML::WikiConverter::PmWiki
+
+HTML::WikiConverter::SnipSnap
+
+HTML::WikiConverter::TikiWiki
+
+HTML::WikiConverter::UseMod
+
+HTML::WikiConverter::WakkaWiki
+
+HTML::WikiConverter::WikkaWiki
+
+=head1 DESCRIPTION
+
+This bundle includes the L<HTML::WikiConverter> distribution as well
+as all available dialect modules needed to convert HTML into a variety
+of wiki markups.
+
+=head1 ACKNOWLEDGEMENTS
+
+Thanks to Tatsuhiko Miyagawa for suggesting that I create this bundle.
+
+=head1 AUTHOR
+
+David J. Iberri, E<lt>diberri at cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<HTML::WikiConverter>
+
+=cut
